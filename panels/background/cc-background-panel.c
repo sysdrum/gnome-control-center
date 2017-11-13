f3fc51f739b9af3624a985f6d0fc565db5268ab3
Julian Sparber <julian@sparber.net>
2017-11-13 16:29:31 +0100

    [refactor] moved gallery item code to grid-item class

--- panels/background/cc-background-panel.c
+++ panels/background/cc-background-panel.c
@@ -162,88 +162,6 @@ get_save_path (const char *filename)
                            NULL);
 }
 
-static void
-add_slideshow_emblem (GdkPixbuf *pixbuf,
-                      gint w,
-                      gint h,
-                      gint scale_factor)
-{
-  GdkPixbuf *slideshow_emblem;
-  GIcon *icon = NULL;
-  GtkIconInfo *icon_info = NULL;
-  GError *error = NULL;
-  GtkIconTheme *theme;
-
-  int eh;
-  int ew;
-  //int h;
-  //int w;
-  int x;
-  int y;
-
-  icon = g_themed_icon_new ("slideshow-emblem");
-  theme = gtk_icon_theme_get_default ();
-  icon_info = gtk_icon_theme_lookup_by_gicon_for_scale (theme,
-                                                        icon,
-                                                        16,
-                                                        scale_factor,
-                                                        GTK_ICON_LOOKUP_FORCE_SIZE |
-                                                        GTK_ICON_LOOKUP_USE_BUILTIN);
-  if (icon_info == NULL) {
-    g_warning ("Your icon theme is missing the slideshow-emblem icon, "
-               "please file a bug against it");
-  }
-  else {
-
-    slideshow_emblem = gtk_icon_info_load_icon (icon_info, &error);
-    if (slideshow_emblem == NULL) {
-      g_warning ("Failed to load slideshow emblem: %s", error->message);
-      g_error_free (error);
-    }
-    else {
-      eh = gdk_pixbuf_get_height (slideshow_emblem);
-      ew = gdk_pixbuf_get_width (slideshow_emblem);
-      //h = gdk_pixbuf_get_height (pixbuf);
-      //w = gdk_pixbuf_get_width (pixbuf);
-      x = w - ew - 5;
-      y = h - eh - 5;
-
-      gdk_pixbuf_composite (slideshow_emblem, pixbuf, x, y, ew, eh, x, y, 1.0, 1.0, GDK_INTERP_BILINEAR, 255);
-    }
-  }
-
-  g_clear_object (&icon_info);
-  g_clear_object (&icon);
-}
-
-
-
-static GdkPixbuf*
-get_or_create_cached_pixbuf_with_size (CcBackgroundPanel *panel,
-                                       const gint width,
-                                       const gint height,
-                                       CcBackgroundItem  *item)
-{
-  gint scale_factor;
-  GdkPixbuf *pixbuf;
-
-  pixbuf = g_object_get_data (G_OBJECT (item), "pixbuf");
-  if (pixbuf == NULL ||
-      gdk_pixbuf_get_width (pixbuf) != width ||
-      gdk_pixbuf_get_height (pixbuf) != height) {
-    scale_factor = gtk_widget_get_scale_factor (panel);
-    pixbuf = cc_background_item_get_frame_thumbnail (item,
-                                                     panel->thumb_factory,
-                                                     width,
-                                                     height,
-                                                     scale_factor,
-                                                     -2, TRUE);
-    g_object_set_data_full (G_OBJECT (item), "pixbuf", pixbuf, g_object_unref);
-  }
-
-  return pixbuf;
-}
-
 static GdkPixbuf*
 get_or_create_cached_pixbuf (CcBackgroundPanel *panel,
                              GtkWidget         *widget,
@@ -287,55 +205,6 @@ on_preview_draw (GtkWidget         *widget,
 
   return TRUE;
 }
-
-static gboolean
-on_gallery_item_draw (GtkWidget         *widget,
-                      cairo_t           *cr,
-                      GdkPixbuf *pixbuf)
-{
-  //GdkPixbuf *pixbuf;
-  const gint space_width = gtk_widget_get_allocated_width (widget);
-  const gint space_height = gtk_widget_get_allocated_height ( (widget));
-  const gint pixbuf_width = gdk_pixbuf_get_width (pixbuf);
-  const gint pixbuf_height = gdk_pixbuf_get_height (pixbuf);
-  const gint scale_factor = gtk_widget_get_scale_factor (widget);
-  gint new_width;
-  gint new_height;
-
-  if (space_width * 9/16 > space_height) {
-    new_width = space_width;
-    new_height = space_width * 9/16;
-  }
-  else {
-    new_width = space_height * 16/9;
-    new_height = space_height;
-  }
-
-  /* pixbuf = get_or_create_cached_pixbuf_with_size (panel,
-     new_width,
-     new_height,
-     cc_background_grid_item_get_ref (gtk_widget_get_parent(widget)));
-
-*/
-
-  pixbuf = gdk_pixbuf_scale_simple (pixbuf,
-                                    new_width,
-                                    new_height,
-                                    GDK_INTERP_BILINEAR);
-
-  if (cc_background_item_changes_with_time (cc_background_grid_item_get_ref (gtk_widget_get_parent(widget)))) {
-    add_slideshow_emblem (pixbuf, (space_width + new_width) / 2, (space_height + new_height)/2, scale_factor);
-  }
-
-  gdk_cairo_set_source_pixbuf (cr,
-                               pixbuf,
-                               (space_width - new_width) / 2,
-                               (space_height - new_height) / 2);
-  cairo_paint (cr);
-
-  return TRUE;
-}
-
 
 static void
 on_panel_resize (GtkWidget *widget,
@@ -740,10 +609,9 @@ create_gallery_item (gpointer item,
                      gpointer user_data)
 {
   CcBackgroundPanel *panel = user_data;
-  GtkWidget *flow;
-  GtkWidget *widget;
-  GdkPixbuf *pixbuf;
   CcBackgroundItem *self = item;
+  GtkWidget *flow;
+  GdkPixbuf *pixbuf;
   gint scale_factor;
   const gint preview_width = 400;
   const gint preview_height = 400 * 9 / 16;
@@ -756,25 +624,7 @@ create_gallery_item (gpointer item,
                                                    preview_height,
                                                    scale_factor,
                                                    -2, TRUE);
-
-  /*if (cc_background_item_changes_with_time (self)) {
-    add_slideshow_emblem (pixbuf, scale_factor);
-    }
-    */
-  widget = gtk_drawing_area_new ();
-  gtk_widget_set_hexpand(widget, TRUE);
-  gtk_widget_set_vexpand(widget, TRUE);
-  g_signal_connect (G_OBJECT (widget), "draw",
-                    G_CALLBACK (on_gallery_item_draw), pixbuf);
-
-  flow = cc_background_grid_item_new(self);
-
-  gtk_widget_set_size_request (flow, 250, 200);
-  cc_background_grid_item_set_ref (flow, self);
-  gtk_widget_show (flow);
-  gtk_widget_show (widget);
-  gtk_container_add (GTK_CONTAINER (flow), widget);
-
+  flow = cc_background_grid_item_new(self, pixbuf);
   return flow;
 }
 
